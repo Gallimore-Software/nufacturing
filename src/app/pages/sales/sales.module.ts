74be983dac57d73ed68e2ecb60b1502d13be25e9
Ahim-Gillamore <ahim@gallimoresoftware.com>
2024-07-17 17:09:23 +0545

    fixed sales style

--- src/app/pages/sales/sales.module.ts
+++ src/app/pages/sales/sales.module.ts
@@ -5,6 +5,11 @@ import { OrdersNavigationComponent } from './components/orders-navigation/orders
 import { QuotesNavigationComponent } from './components/quotes-navigation/quotes-navigation.component';
 import { SalesDashboardComponent } from './components/sales-dashboard/sales-dashboard.component';
 import { SalesRoutingModule } from './sales-routing.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -13,6 +18,14 @@ import { SalesRoutingModule } from './sales-routing.module';
     OrdersNavigationComponent,
     QuotesNavigationComponent,
   ],
-  imports: [CommonModule, SalesRoutingModule],
+  imports: [
+    CommonModule, 
+    SalesRoutingModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
+    MatChipsModule
+  ],
 })
 export class SalesModule {}
