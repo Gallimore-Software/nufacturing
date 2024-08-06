256208385e89546f254b5420d1e8ecedd7bf7f48
Nicholas Gallimore <nick@gallimoresoftware.com>
2024-08-06 16:30:50 -0400

    NFG-67 Add tests for view inventory

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
-      }
-    },
-  };
-  
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e/'],
+};
