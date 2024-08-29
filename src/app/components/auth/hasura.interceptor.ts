820f53b6137d0a825d605d7619115b743d839845
Ahim-Gillamore <ahim@gallimoresoftware.com>
2024-08-29 16:24:29 +0545

    NFG-86 temporary webflow control and added prod environment for live API

--- src/app/components/auth/hasura.interceptor.ts
+++ src/app/components/auth/hasura.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environment/environment';
+import { environment } from 'src/environment/environment.prod';
 
 @Injectable()
 export class HasuraInterceptor implements HttpInterceptor {
