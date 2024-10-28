9a2951917c9d16a236cb334751cc205941eff55a
unknown <chris@gallimoresoftware.com>
2024-10-28 11:50:18 +0800

    NFG-86 updated tests and package.json

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,24 +3,25 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e/'],
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
-  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'], // Points to Jest setup file
+  globalSetup: 'jest-preset-angular/global-setup', // Required for Angular-specific setup
+  testEnvironment: 'jsdom', // Set up DOM environment for browser-like tests
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.html$',
-      // Remove outdated transformers as they are handled automatically in recent versions of jest-preset-angular
+      tsconfig: 'tsconfig.spec.json', // Points to TypeScript config for tests
+      stringifyContentPathRegex: '\\.html$', // Handles HTML templates in Angular components
     },
   },
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  extensionsToTreatAsEsm: ['.ts'], // Treat TypeScript files as ES modules
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'], // Supported file extensions
   transform: {
-    '^.+\\.(ts|html)$': 'ts-jest',
+    '^.+\\.(ts|html)$': 'ts-jest', // Use ts-jest for TypeScript and HTML files
   },
-  transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!@ngrx|@angular|jest-preset-angular)',
+  ], // Ensures Jest processes ES modules in node_modules
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
+    '^src/(.*)$': '<rootDir>/src/$1', // Custom path mappings
     '^@env/(.*)$': '<rootDir>/src/environments/$1',
   },
 };
