6b81b5ccc0879d8efe5b41b8caffa5186ccf6818
unknown <chris@gallimoresoftware.com>
2024-10-28 09:09:29 +0800

    NFG-86 Add lint and package.json changes

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,31 +1,26 @@
-// jest.config.js
 module.exports = {
-  preset: "jest-preset-angular",
+  preset: 'jest-preset-angular',
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/e2e/"],
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  globalSetup: "jest-preset-angular/global-setup",
-  testEnvironment: "jsdom",
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/e2e/'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'], // The setup-jest file should be configured properly
+  globalSetup: 'jest-preset-angular/global-setup',
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'], // Treat TypeScript files as ESModules
   globals: {
-    "ts-jest": {
-      tsconfig: "tsconfig.spec.json",
-      stringifyContentPathRegex: "\\.html$",
-      astTransformers: {
-        before: [
-          "jest-preset-angular/build/InlineFilesTransformer",
-          "jest-preset-angular/build/StripStylesTransformer",
-        ],
-      },
+    'ts-jest': {
+      useESM: true, // Enable ESM in ts-jest
+      tsconfig: 'tsconfig.spec.json', // Ensure the correct tsconfig is used
+      stringifyContentPathRegex: '\\.html$',
     },
   },
-  moduleFileExtensions: ["ts", "html", "js", "json"],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
   transform: {
-    "^.+\\.(ts|html)$": "ts-jest",
+    '^.+\\.(ts|html)$': 'ts-jest', // Transform TypeScript and HTML files with ts-jest
   },
-  transformIgnorePatterns: ["node_modules/(?!@ngrx)"],
+  transformIgnorePatterns: ['node_modules/(?!@ngrx)'],
   moduleNameMapper: {
-    "^src/(.*)$": "<rootDir>/src/$1",
-    "^@env/(.*)$": "<rootDir>/src/environments/$1",
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^@env/(.*)$': '<rootDir>/src/environments/$1',
   },
 };
